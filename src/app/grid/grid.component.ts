107b35b08dc3668b091c1c1ebe98a0aee5106344
IsYet1 <Don@IsYet.com>
2021-06-27 18:02:06 -0500

    External filter is working

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store, select } from '@ngrx/store';
+import { AppState } from 'src/app/appState/index';
+import * as appSelectors from 'src/app/appState/app.selectors';
+
+
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
@@ -7,11 +12,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridComponent implements OnInit {
 
-  columnDefs = [
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price'}
+columnDefs = [
+  { field: 'make' },
+  { field: 'model' },
+  { field: 'price'}
 ];
+
+filterText = '';
 
 rowData = [
     { make: 'Toyota', model: 'Celica', price: 35000 },
@@ -19,9 +26,32 @@ rowData = [
     { make: 'Porsche', model: 'Boxter', price: 72000 }
 ];
 
-constructor() { }
+private gridApi: any;
+// doesExternalFilterPass: boolean = false;
+// isExternalFilterPresent: boolean = false;
+
+constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
   }
+
+  onGridReady(params: any) {
+    this.gridApi = params.api;
+    // this.gridColumnApi = params.columnApi;
+
+    this.store.pipe(select(appSelectors.getFilterText)).subscribe(
+      filterText => {
+        this.filterText = filterText;
+        this.gridApi.onFilterChanged();
+      }
+    )
+
+  }
+
+  isExternalFilterPresent = (): boolean =>
+    this.filterText.length > 0
+
+  doesExternalFilterPass = (node: any): boolean =>
+    (node.data['make'] as string).includes(this.filterText)
 
 }
